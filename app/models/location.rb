efaa2926823c7d4ef2624a23f27ba44c98605947
Mayowa Pitan <mayowa.pitan@andela.com>
2016-11-26 15:03:28 +0100

    remove unnecessary roads from database

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,7 +2,8 @@ class Location < ActiveRecord::Base
   attr_accessor :previous_location
   attr_writer :distance, :visited, :road_by
 
-  has_many :roads, foreign_key: :origin_location_id, dependent: :destroy
+  has_many :road_from, class_name: Road, foreign_key: :origin_location_id, dependent: :destroy
+  has_many :road_to, class_name: Road, foreign_key: :destination_location_id, dependent: :destroy
 
   def distance
     @distance ||= Fixnum::MAX
